0e38da0c93a0f44dc7fe0c262af01c256c5dca0f
Yamahdi@1984 <fatemeh.kamali198422@gmail.com>
2022-07-09 02:16:19 +0430

    tamrin1

---
+++ sum1 ta n/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.WriteLine("please enter a number:");
+int a = int.Parse(Console.ReadLine()) ;
+int zegma = 0;
+ for (var i = 1; i <= a; i++)
+ {
+    zegma = zegma+i;
+ }
+ System.Console.WriteLine("zegma=" + zegma);
